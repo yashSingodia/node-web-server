62e7104fbc4eb2708476b644d7d54d8a62a021f7
yashSingodia <yashsingodia@gmail.com>
2018-11-06 20:43:17 +0530

    Included Projects Tab

--- server.js
+++ server.js
@@ -55,6 +55,12 @@ app.get('/about', (req,res) => {
     });
 });
 
+app.get('/projects', (req,res) => {
+    res.render('projects.hbs',{
+        pageTitle: 'Projects'
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
